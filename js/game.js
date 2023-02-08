c54cf227639f9c671f26593ee00c9030d3d02d0c
dominictoms <dominictoms9997@gmail.com>
2023-02-08 08:26:51 +0000

    added secret game

---
+++ js/game.js
@@ -0,0 +1,220 @@
+/* define the sprites here  */
+let char = [
+	'            ^__^',
+	'    _______/(oo)',
+	'/\\/(       /(__)',
+	'   | w----||    ',
+	'   ||     ||    '
+];
+
+let obstacle = [
+	'  |  ',
+	'(_|_)',
+	'  |  '
+];
+
+let stones = ['=', '-'];
+let ground = '';
+
+let len = 60;      /* length of road */
+let dist = 100;    /* distance from obstacle */
+let airtime = 0;   /* time player has left airborne */
+let dead = false;  /* is the player dead? */
+let score = 0;     /* total points earned by player */
+let speed = 1;     /* speed of gameplay */
+let tic = 0;       /* tic counter used for leg animation */
+let fps = 30;      /* how many frames to render a second */
+let frame = 0;     /* frame of leg animation */
+let awoke = false; /* has the game been started? */
+
+let element;
+
+function draw()
+{
+	/* string to store current frame */
+	let out = '';
+
+	/* frame for leg animation */
+	let legs = '||     ||';
+
+	/* if player isn't jumping */
+	if (airtime <= 0)
+	{
+		/* die on collision */
+		if (dist < char[0].length)
+		{
+			/* death sprite */
+			char[1] = char[1].substring(0, 13) + 'xx' + char[1].substring(13 + 2);
+			char[3] = char[3].substring(0, 14) + 'U' + char[3].substring(14 + 1);
+
+			/* keep player airborne if died above obstacle */
+			if (dist < char[0].length - speed)
+			{
+				airtime = 1;
+			}
+
+			dead = true;
+		}
+
+		/* don't do these things if dead and airborne */
+		if (!(dead && airtime))
+		{
+			/* animate legs on ground */
+			if (!frame)
+			{
+				legs = '|╵     |╵';
+			}
+
+			else
+			{
+				legs = '╵|     ╵|';
+			}
+
+			/* add whitespace to account for jump height */
+			out += '<br>'.repeat(obstacle.length);
+		}
+	}
+
+	/* apply leg animation */
+	char[4] = char[4].substring(0, 3) + legs + char[4].substring(3 + 9);
+
+	/* iterate through character sprite line by line */
+	for (let i = 0; i < char.length; i++)
+	{
+		/* add current line of character to output */
+		out += char[i];
+
+		/* add obstacle on same line if player isn't jumping */
+		if (i >= char.length - obstacle.length && airtime <= 0)
+		{
+			let space = dist - char[i].length;
+			if (space <= 0)
+			{
+				space = 0;
+			}
+
+			out += ' '.repeat(space) + obstacle[i - char.length + obstacle.length];
+		}
+
+		out += '<br>';
+	}
+
+	/* draw obstacle below player if jumping */
+	if (airtime > 0)
+	{
+		for (let i = 0; i < obstacle.length; i++)
+		{
+			/* allow to seamlessly roll off canvas */
+			space = dist;
+			cutoff = 0;
+			if (dist <= 0)
+			{
+				space = 0;
+				cutoff = Math.abs(dist);
+			}
+
+			out += ' '.repeat(space);
+			out += obstacle[i].substr(cutoff, obstacle[i].length) + '<br>';
+		}
+	}
+
+	out += ground + '<br>' + ' score: ' + score;
+	return out;
+}
+
+
+function update()
+{
+	if (!dead)
+	{
+		/* update frame counter */
+		tic++;
+
+		/* scroll the obstacle towards the player */
+		dist -= speed;
+
+		/* increase score for every frame alive */
+		score++;
+
+		/* speed up game every 1000 points */
+		if (speed <= 3)
+		{
+			speed = Math.ceil(score / 1000);
+		}
+
+		/* update and draw the ground */
+		ground = ground.substring(speed);
+		for (let i = 0; i < speed; i++)
+		{
+			ground += stones[Math.floor(Math.random() * stones.length)];
+		}
+
+		/* decrease players airtime if jumping */
+		if (airtime > 0)
+		{
+			airtime--;
+		}
+
+		/* reset distance when obstacle goes off screen */
+		if (dist <= -obstacle[0].length)
+		{
+			dist = len * 1.75 + Math.floor(Math.random() * 30);
+		}
+
+		/* update frame */
+		if (tic > Math.ceil(fps / (speed + 1)))
+		{
+			tic = 0;
+			frame = 1 - frame;
+		}
+	}
+
+	/* update the game canvas */
+	element.innerHTML = draw().replace(/ /g, '&nbsp;');
+}
+
+function awake()
+{
+	/* can't wake the game up twice */
+	if (awoke)
+	{
+		return;
+	}
+
+	awoke = true;
+
+	/* get game canvas */
+	element = document.getElementById('game');
+
+	/* generate the initial ground texture */
+	for (let i = 0; i < len; i++)
+	{
+		ground += stones[Math.floor(Math.random() * stones.length)];
+	}
+
+	/* begin the update loop */
+	setInterval(update, 1000/fps);
+}
+
+onkeyup = function(e)
+{
+	if (e.code == "Space" && !dead)
+	{
+		/* wake game with spacebar */
+		if (!awoke)
+		{
+			awake();
+		}
+
+		/* otherwise jump with space */
+		else
+		{
+			if (airtime <= 0)
+			{
+				airtime = 30;
+			}
+		}
+	}
+}
+
+//awake();
---
+++ js/randomtext.js
@@ -0,0 +1,83 @@
+/* function for calculating age */
+function calculateAge(dob)
+{ 
+	return new Date(Date.now() - dob.getTime()).getFullYear() - 1970;
+}
+
+/* function for getting new phrase */
+function getPhrase(phrases)
+{
+	return phrases[Math.floor(Math.random() * phrases.length)];
+}
+
+/* function for updating text */
+function updateText(text, phrases, history, historyLen)
+{
+	/* get a new phrase */
+	let phrase = getPhrase(phrases);
+
+	/* ensure phrase isn't in history /*
+	while (history.indexOf(phrase) != -1)
+	{
+		phrase = getPhrase(phrases);
+	}
+
+	/* add current phrase to history */
+	history.unshift(phrase);
+
+	/* lets forget history */
+	if (history.length > historyLen)
+	{
+		history.pop();
+	}
+
+	/* update the text */
+	text.innerHTML = phrase;
+}
+
+/* run function on page load */
+window.onload = () =>
+{
+	/* array of text to display */
+	const phrases =
+	[
+		calculateAge(new Date(2000, 11, 3)) + ' year old from cambridge',
+		'brainfuck extrodonaire',
+		'command line wizard',
+		'computer science student',
+		'dedicated vegetarian',
+		'devoted unix fan',
+		'full stack developer',
+		'playstation 2 player in ' + new Date().getFullYear(),
+		'80s anime enthusiast',
+		'downhill longboarder',
+	];
+
+	/* array to store history */
+	let history = [];
+
+	/* int to store history length */
+	const historyLen = Math.ceil(phrases.length/2);
+
+	/* variable to store text element */
+	let text = document.getElementById('typewriter');
+
+	/* set first text value */
+	updateText(text, phrases, history, historyLen);
+
+	/* enable looping animation if end can be detected */
+	if (text.onanimationiteration !== undefined)
+	{
+		text.style.animationIterationCount = 'infinite';
+	}
+
+	/* run on animation loop */
+	text.onanimationiteration = (anim) =>
+	{
+		/* update text if typing animation */
+		if (anim.animationName == 'typing')
+		{
+			updateText(text, phrases, history, historyLen);
+		}
+	};
+}
