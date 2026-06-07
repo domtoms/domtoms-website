6764d34752b680a4ab0fe67b633a8c207a55bd56
domtoms <39525556+domtoms@users.noreply.github.com>
2026-06-07 21:42:09 +0100

    design overhaul

---
+++ js/cow.js
@@ -0,0 +1,101 @@
+const cow = [
+	'            ^__^',
+	'    _______/(oo)',
+	'/\\/(       /(__)',
+	'   | w----||',
+	'   ||     ||'
+];
+
+const cactus = [
+	'  |  ',
+	'(_|_)',
+	'  |  '
+];
+
+const stones = ['=', '-'];
+
+const len = 40;
+const fps = 30;
+
+let ground = '';
+let pos = 80;
+let airtime = 0;
+let frame = 0;
+let tic = 0;
+
+for (let i = 0; i < len; i++) {
+	ground += stones[Math.floor(Math.random() * stones.length)];
+}
+
+function draw() {
+	let out = '';
+
+	let legs = '||     ||';
+	if (!airtime) {
+		legs = frame ? '╵|     ╵|' : '|╵     |╵';
+		out += '<br>'.repeat(cactus.length);
+	}
+	cow[4] = '   ' + legs;
+
+	for (let i = 0; i < cow.length; i++) {
+		out += cow[i];
+		if (!airtime && i >= cow.length - cactus.length) {
+			let space = pos - cow[i].length;
+			if (space < 0) {
+				space = 0;
+			}
+
+			out += ' '.repeat(space);
+			out += cactus[i - cow.length + cactus.length];
+		}
+		out += '<br>';
+	}
+
+	if (airtime) {
+		for (let i = 0; i < cactus.length; i++) {
+			let space = pos;
+			let left = 0;
+			if (pos <= 0) {
+				space = 0;
+				left = -pos;
+			}
+
+			out += ' '.repeat(space);
+			out += cactus[i].substring(left);
+			out += '<br>';
+		}
+	}
+
+	out += ground;
+	return out;
+}
+
+function update() {
+	if (!airtime && pos === 25) {
+		airtime = 30;
+	}
+
+	if (airtime) {
+		airtime--;
+	}
+
+	pos--;
+
+	if (pos <= -cactus[0].length) {
+		pos = len + 20 + Math.floor(Math.random() * 20);
+	}
+
+	ground = ground.substring(1);
+	ground += stones[Math.floor(Math.random() * stones.length)];
+
+	tic++;
+
+	if (tic >= fps / 2) {
+		tic = 0;
+		frame = 1 - frame;
+	}
+
+	game.innerHTML = draw().replace(/ /g, '&nbsp;');
+}
+
+setInterval(update, 1000 / fps);
---
+++ js/typewriter.js
@@ -0,0 +1,56 @@
+const yearsSince = date => {
+	const today = new Date();
+	let years = today.getFullYear() - date.getFullYear();
+
+	if (
+		today.getMonth() < date.getMonth() ||
+		(today.getMonth() === date.getMonth() && today.getDate() < date.getDate())
+	) {
+		years--;
+	}
+
+	return years;
+};
+
+const phrases = [
+	`${yearsSince(new Date(2000, 10, 3))} year old from cambridge`,
+	'brainfuck extraordinaire',
+	'command line wizard',
+	'flac hoarder',
+	'jungle and techno dj',
+	'london based swe',
+	`vegetarian of ${yearsSince(new Date(2021, 4, 19))} years`
+];
+
+window.onload = () => {
+	const text = document.getElementById('typewriter');
+	const history = [];
+	const historyLen = Math.ceil(phrases.length / 2);
+
+	const update = () => {
+		let phrase;
+		do {
+			phrase = phrases[Math.floor(Math.random() * phrases.length)];
+		} while (history.includes(phrase));
+
+		history.unshift(phrase);
+		if (history.length > historyLen) {
+			history.pop();
+		}
+
+		text.innerHTML = phrase;
+	};
+
+	update();
+
+	if (text.onanimationiteration !== undefined) {
+		text.style.animationIterationCount = 'infinite';
+	}
+
+	text.onanimationiteration = e => {
+		if (e.animationName === 'typing') {
+			update();
+		}
+	};
+};
+
