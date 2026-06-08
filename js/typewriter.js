3ae3c19a5a7cf0e83b2a311ecb5aa439fd3295e5
domtoms <39525556+domtoms@users.noreply.github.com>
2026-06-08 16:17:28 +0100

    typewriter tweaks

--- js/typewriter.js
+++ js/typewriter.js
@@ -13,7 +13,6 @@ const yearsSince = date => {
 
 const phrases = [
 	`${yearsSince(new Date(2000, 10, 3))} year old from cambridge`,
-	'brainfuck extraordinaire',
 	'command line wizard',
 	'flac hoarder',
 	'jungle and techno dj',
@@ -21,7 +20,8 @@ const phrases = [
 	`ps2 player in ${new Date().getFullYear()}`,
 	`swe @ jagex`,
 	`uea graduate`,
-	`vegetarian of ${yearsSince(new Date(2021, 4, 19))} years`
+	`vegetarian of ${yearsSince(new Date(2021, 4, 19))} years`,
+	'vim purist'
 ];
 
 window.addEventListener("load", () => {
@@ -42,6 +42,9 @@ window.addEventListener("load", () => {
 
 		text.innerHTML = phrase;
 	};
+
+	text.style.animation = 'typing 6s linear infinite';
+	text.style.width = '0';
 
 	update();
 
